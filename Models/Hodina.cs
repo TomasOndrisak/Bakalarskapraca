206b63b3cf8ce26d3f12202df91f7a24e669a046
Tomas <tomas.ondrisak@gmail.com>
2022-05-05 22:12:05 +0200

    First commit

---
+++ Models/Hodina.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WASMrozvrh.Models
+{
+    public class Hodina
+    {
+        [Key] // primarny kluc
+        public int Id { get; set; }
+        [Required] // povinne pole nemôže vyjsť z konštruktora prázdne
+        public string Nazov { get; set; }
+       
+
+    }
+}
---
+++ Models/HodinaModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace WASMrozvrh.Models
+{
+    public class HodinaModel
+    {
+        public int Id { get; set; }
+        public HodinaStatuses Status { get; set; }
+        public string NazovHodiny { get; set; }
+    }
+
+    public enum HodinaStatuses
+    {
+        P1,P2,P3,P4,P5,P6,
+        U1,U2,U3,U4,U5,U6,
+        S1,S2,S3,S4,S5,S6,
+        Stv1,Stv2,Stv3,Stv4,Stv5,Stv6,
+        Pi1,Pi2,Pi3,Pi4,Pi5,Pi6
+    }
+}
